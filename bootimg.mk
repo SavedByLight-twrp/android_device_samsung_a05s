fdfbe0ee15d9623c300d4e0863789715909c40ea
Jamie Falconer <mrfluffyoven@gmail.com>
2025-05-17 12:23:18 +0100

    Update bootimg.mk

--- bootimg.mk
+++ bootimg.mk
@@ -15,4 +15,4 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_k
         $(hide) $(AVBTOOL) add_hash_footer --image $@ --partition_size $(BOARD_RECOVERYIMAGE_PARTITION_SIZE) --partition_name recovery --algorithm $(BOARD_AVB_RECOVERY_ALGORITHM) --key $(BOARD_AVB_RECOVERY_KEY_PATH)
         @echo "Made recovery image: $@"
         $(hide) tar -C $(PRODUCT_OUT) -c recovery.img > $(FLASH_IMAGE_TARGET)
-        @echo "Made flashable $(FLASH_IMAGE_TARGET): $@"
+        @echo "Made flashable $(FLASH_IMAGE_TARGET): $@" 
